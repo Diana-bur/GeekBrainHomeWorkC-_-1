8d841fcdc6aee2292fa96dee229ee046a92777f2
Diana <BDianaE@yandex.ru>
2023-04-26 21:24:46 +0700

    Решение 4 задачи - найдено

--- Homework1/Task_4/Program.cs
+++ Homework1/Task_4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите положительное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int num = 2; // Так как отсчёт от 1, а выводим только чётные числа
+
+if (max >= 2)
+{
+    while (num <= max)
+    {
+        if (num%2==0)
+        {
+            Console.Write(num + ", ");
+        }
+        num++;
+    }
+}
+else
+Console.WriteLine("Здесь нет чётных положительных чисел");
